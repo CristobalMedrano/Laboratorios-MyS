315e8ebb2e43bde81b964c7a69ec1fc8e51b6ff8
Cristobal <cristobal.medrano@usach.cl>
2021-05-06 18:52:45 -0400

    Ejemplos visuales actualizados

---
+++ functions/show_std.m
@@ -0,0 +1,9 @@
+% Función: Muestra el resultado de la desviación estandar
+% Entrada: Valor de la desviación estandar, titulo del ejemplo
+% Salida: No posee
+function show_std(std_value, title)
+    
+fprintf('\n%s\n', title)
+fprintf('Desviación estandar obtenida: %f\n', std_value)
+
+end
