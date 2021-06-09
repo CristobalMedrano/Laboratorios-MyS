21f00fcd69df3307253e2f0d77108f72b6ef750e
Cristobal Medrano <cristobal.medrano@usach.cl>
2021-06-08 20:17:30 -0400

    Se actualizan nombres de variables
    Se suben los enunciados de cada laboratorio

--- Laboratorio 2/Laboratorio2.m
+++ Laboratorio 2/Laboratorio2.m
@@ -19,39 +19,55 @@ s = tf('s');
 %% Parte 1
 % Se definen las funciones de transferencia de la función 1 y 2.
 % Función 1
-H1 = 4*s / (3*s+1);
-H2 = feedback(H1,1);
+H1_open = (4*s)/(3*s+1);
+H1_close = feedback(H1_open, 1, -1);
 
 % Función 2
-H3 = (5*s^2 + 7*s + 1)/(s^2 + 6*s + 3);
-H4 = feedback(H3, 1);
+H2_open = (5*s^2 + 7*s + 1)/(s^2 + 6*s + 3);
+H2_close = feedback(H2_open, 1);
 
 % Se obtienen los datos de la función de transferencia.
-[ func_1_gain, func_1_stime, func_1_zeros, func_1_poles] = getDataTransferFunction(H1);
-[ func_2_gain, func_2_stime, func_2_zeros, func_2_poles] = getDataTransferFunction(H2);
-[ func_3_gain, func_3_stime, func_3_zeros, func_3_poles] = getDataTransferFunction(H3);
-[ func_4_gain, func_4_stime, func_4_zeros, func_4_poles] = getDataTransferFunction(H4);
+[ H1_open_gain, ...
+    H1_open_stime, ...
+    H1_open_zeros, ...
+    H1_open_poles] = getDataTransferFunction(H1_open);
+
+[ H1_close_gain, ...
+    H1_close_stime, ...
+    H1_close_zeros, ...
+    H1_close_poles] = getDataTransferFunction(H1_close);
+
+[ H2_open_gain, ...
+    H2_open_stime, ...
+    H2_open_zeros, ...
+    H2_open_poles] = getDataTransferFunction(H2_open);
+
+[ H2_close_gain, ...
+    H2_close_stime, ...
+    H2_close_zeros, ...
+    H2_close_poles] = getDataTransferFunction(H2_close);
 
 %Graficos para funcion de transferencia (TF = Transfer Function)
-showResponseTFPlot(H1, "Respuesta al escalón para lazo abierto F1");
-showResponseTFPlot(H2, "Respuesta al escalón para lazo cerrado F1");
-showResponseTFPlot(H3, "Respuesta al escalón para lazo abierto F2");
-showResponseTFPlot(H4, "Respuesta al escalón para lazo cerrado F2");
+showResponseTFPlot(H1_open, "Respuesta al escalón para lazo abierto H1");
+showResponseTFPlot(H1_close, "Respuesta al escalón para lazo cerrado H1");
+showResponseTFPlot(H2_open, "Respuesta al escalón para lazo abierto H2");
+showResponseTFPlot(H2_close, "Respuesta al escalón para lazo cerrado H2");
 
 %% Parte 2
 % Se definen las funciones de transferencia a utilizar
-H1 = (4*s)/(10*s^2 + 4);
-H2 = 3/(6*s + 16);
-H3 = (4*s + 10)/(4*s^3 + 9*s^2 + 5*s);
-H4 = 1/(7*s + 10);
-H5 = (8*s + 8)/(s^3 + 2*s^2 + 3*s);
-H6 = (3*s + 2)/(5*s^2 + 7*s + 10);
+function_1 = (4*s)/(10*s^2 + 4);
+function_2 = 3/(6*s + 16);
+function_3 = (4*s + 10)/(4*s^3 + 9*s^2 + 5*s);
+function_4 = 1/(7*s + 10);
+function_5 = (8*s + 8)/(s^3 + 2*s^2 + 3*s);
+function_6 = (3*s + 2)/(5*s^2 + 7*s + 10);
 
 % Se define la función de transferencia según el diagrama
-H = H1 + H2 + (feedback(H3, 1, +1)*(H4 + H5)*H6);
+H = (function_1 + function_2) + ...
+    (feedback(function_3, 1, +1)*(function_4 + function_5)*function_6);
 
 % Se muestra por consola a la función de transferencia
-disp(H);
+H %#ok<NOPTS>
 
 % Se muestra en um gráfico la respuesta al escalon de la función de 
 % transferencia.
